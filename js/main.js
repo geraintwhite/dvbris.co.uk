4f33379197b9bb92bc23c0d9929d1d63ca7fa00a
Geraint White <geraint@android.net>
2014-09-18 21:35:52 +0100

    Added open class to dropdowns

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ $(function() {
   $(document).click(function(e) {
     var target = $(e.target).parent();
     if (!isDesktop()) {
-      $('nav li ul').not(target.find('ul')).slideUp(200);
+      $('nav li:has(ul)').not(target).removeClass('open');
+      $('nav li ul').not(target.children('ul')).slideUp(200);
       if (target.is('nav li:has(ul)')) {
-        target.find('ul').slideToggle(200);
+        target.toggleClass('open');
+        target.children('ul').slideToggle(200);
       }
     }
   })
