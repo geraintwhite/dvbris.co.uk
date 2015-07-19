87f5814df6f7466ead3bf1fddd6b26afad904127
Geraint White <geraintwhite@gmail.com>
2015-07-19 22:49:56 +0100

    Use gulp-template-html instead of DreamWeaver

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,56 @@
 var gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    htmlmin = require('gulp-htmlmin'),
     sass = require('gulp-sass'),
-    prefix = require('gulp-autoprefixer'),
-    livereload = require('gulp-livereload');
+    template = require('gulp-template-html'),
+    minify = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer');
 
 
-gulp.task('css', function () {
-  return gulp.src('./css/main.scss')
+gulp.task('css', function() {
+  return gulp.src('./src/css/main.scss')
     .pipe(sass())
     .pipe(prefix())
-    .pipe(gulp.dest('./css/'));
+    .pipe(minify())
+    .pipe(gulp.dest('./build/css/'));
 });
 
-gulp.task('reload', function () {
-  setTimeout(function () { livereload.changed('/'); }, 1000);
+gulp.task('html', function() {
+  return gulp.src('./src/content/*.html')
+    .pipe(template('./src/templates/template.html'))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeOptionalTags: true
+    }))
+    .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('watch', function () {
+gulp.task('copy', function() {
+  gulp.src('./src/css/*.css')
+    .pipe(gulp.dest('./build/css/'));
+  gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./build/js/'));
+  gulp.src('./src/images/**/*')
+    .pipe(gulp.dest('./build/images/'));
+  gulp.src(['./src/favicon.ico', './src/mail.php'])
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('reload', function() {
+  setTimeout(function() { livereload.changed('/'); }, 1000);
+});
+
+gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('./css/*.scss', ['css']);
+  gulp.watch(['./src/css/*.css', './src/js/*.js', './src/images/*'], ['copy']);
   gulp.watch('./**/*', ['reload']);
 });
 
-gulp.task('default', ['css']);
+gulp.task('default', ['css', 'html', 'copy']);
--- src/js/main.js
+++ src/js/main.js
@@ -32,7 +32,7 @@ $(function() {
         $(nav_ul).slideToggle(anim_delay);
       }
     }
-  })
+  });
 
   $(window).resize(function() {
     $('body').toggleClass(desktop_class, isDesktop());
@@ -45,12 +45,17 @@ $(function() {
     }
   });
 
+  $('a[data-lightbox]').each(function() {
+    $(this).attr('data-title', $(this).siblings('p').html());
+  });
+
   if (!Modernizr.svg) {
     $('.logo').attr('src', 'images/dvbris.png');
   }
 
   $(window).resize();
 });
+
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
